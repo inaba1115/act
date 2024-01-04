d5e88f2a94298e5e68635683d7d2b7887d6ef5a3
inaba1115 <inaba1115@gmail.com>
2024-01-05 02:55:31 +0900

    refactor

--- act/bpm.py
+++ act/bpm.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 class BPM:
     def __init__(self, bpm: float) -> None:
         self._bpm = bpm
-        self._sec_per_beat = 60 / bpm
         self._sec_per_bar = 60 / bpm * 4
 
     def __repr__(self) -> str:
@@ -17,8 +16,5 @@ class BPM:
     def bpm(self) -> float:
         return self._bpm
 
-    def bar(self) -> float:
-        return self._sec_per_bar
-
-    def div(self, n: int) -> float:
+    def div(self, n: float) -> float:
         return self._sec_per_bar / n
---
+++ tests/test_bpm.py
@@ -0,0 +1,25 @@
+import unittest
+
+from act.bpm import BPM
+
+
+class TestBPM(unittest.TestCase):
+    def test_bpm(self):
+        self.assertAlmostEqual(BPM(80).bpm(), 80, delta=0.01)
+        self.assertAlmostEqual(BPM(120).bpm(), 120, delta=0.01)
+        self.assertAlmostEqual(BPM(145).bpm(), 145, delta=0.01)
+        self.assertAlmostEqual(BPM(180).bpm(), 180, delta=0.01)
+
+    def test_div(self):
+        self.assertAlmostEqual(BPM(80).div(1), 3, delta=0.01)
+        self.assertAlmostEqual(BPM(80).div(1.5), 2, delta=0.01)
+        self.assertAlmostEqual(BPM(80).div(3), 1, delta=0.01)
+        self.assertAlmostEqual(BPM(120).div(1), 2, delta=0.01)
+        self.assertAlmostEqual(BPM(120).div(1.5), 1.33, delta=0.01)
+        self.assertAlmostEqual(BPM(120).div(3), 0.66, delta=0.01)
+        self.assertAlmostEqual(BPM(145).div(1), 1.65, delta=0.01)
+        self.assertAlmostEqual(BPM(145).div(1.5), 1.1, delta=0.01)
+        self.assertAlmostEqual(BPM(145).div(3), 0.55, delta=0.01)
+        self.assertAlmostEqual(BPM(180).div(1), 1.33, delta=0.01)
+        self.assertAlmostEqual(BPM(180).div(1.5), 0.88, delta=0.01)
+        self.assertAlmostEqual(BPM(180).div(3), 0.44, delta=0.01)
